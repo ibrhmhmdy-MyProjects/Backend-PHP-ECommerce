5f776afe37b720ebc4b0788573ce59d65a51750f
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-07-09 22:03:05 +0300

    Code Signup and Login

--- login.php
+++ login.php
@@ -2,6 +2,55 @@
 include "header.php";
 include "navbar.php";
 include "dbConnection.php";
+
+if(isset($_POST['login'])){
+  extract($_POST);
+  // Check Form Validation 
+  $errors = [];
+  // Email => [required - email - Check Valid]
+  if($email == ""){
+    $errors[] = "Please Email Address is Required";
+  }elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    $errors[] = "Please Email Address is Not Valid";
+  }else{
+    $query = "SELECT email FROM users WHERE email = '$email'";
+    $check_email = mysqli_query($conn,$query);
+    $result = mysqli_num_rows($check_email);
+    if(!$result > 0){
+      $errors[] = "Email Address is not Exists";
+    }
+  }
+  // Password => [required - Min 4 - Check Valid]
+  if($password == ""){
+    $errors[] = "Please Password is Required";
+  }elseif(strlen($password) < 4){
+    $errors[] = "Please Password Must be 4 Chars or More";
+  }else{
+    $query = "SELECT * FROM users WHERE email = '$email'";
+    $check_password = mysqli_query($conn,$query);
+    $rows = mysqli_fetch_all($check_password);
+    foreach($rows as $row){
+      $hash_password = $row['password'];
+      echo $hash_password;
+    }
+    if(!password_verify($password,$hash_password)){
+      $errors[] = "Password is invalid";
+    }
+  }
+
+  // Check and Show Messages
+  if(!empty($errors)){
+    // Show Errors
+    foreach($errors as $error){
+      echo "<div class='alert alert-warning alert-dismissible fade show' role='alert' >
+        $error
+        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+      </div>";
+    }
+  }else{
+    header("location: index.php");
+  }
+}
 ?>
 
 <div class="card-body px-5 py-5" style="background-color:darkgray;">
